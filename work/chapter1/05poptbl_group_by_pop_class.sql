8218bd94cc811a64ca910ea7a41c102ed0b6c82c
owsbj <owsbj1011@gmail.com>
2021-08-14 22:24:13 +0900

    create poptbl group by pop class

---
+++ work/chapter1/05poptbl_group_by_pop_class.sql
@@ -0,0 +1,25 @@
+SELECT
+  CASE
+    WHEN population < 100 THEN '01'
+    WHEN population >= 100
+    AND population < 200 THEN '02'
+    WHEN population >= 200
+    AND population < 300 THEN '03'
+    WHEN population >= 300 THEN '04'
+    ELSE NULL
+  END AS pop_class,
+  count(*) AS cnt
+FROM
+  PopTbl
+GROUP BY
+  CASE
+    WHEN population < 100 THEN '01'
+    WHEN population >= 100
+    AND population < 200 THEN '02'
+    WHEN population >= 200
+    AND population < 300 THEN '03'
+    WHEN population >= 300 THEN '04'
+    ELSE NULL
+  END
+ORDER BY
+  pop_class;
