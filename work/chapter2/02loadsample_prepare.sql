cd4355d56fdf1532009839f11a8c143a803525a1
owsbj <owsbj1011@gmail.com>
2021-08-17 23:08:36 +0900

    prepare

---
+++ work/chapter2/02loadsample_prepare.sql
@@ -0,0 +1,11 @@
+CREATE TABLE loadsample (sample_date date PRIMARY KEY, load_val int);
+
+INSERT INTO
+  loadsample
+VALUES
+  ('2018-02-01', 1024),
+  ('2018-02-02', 2366),
+  ('2018-02-05', 2366),
+  ('2018-02-07', 985),
+  ('2018-02-08', 780),
+  ('2018-02-12', 1000);
